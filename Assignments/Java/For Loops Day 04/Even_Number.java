54c582786e0e91214691d98b8de041801d644ed3
Jyotiraditya Patil <99524036+jyotiradityapatil123@users.noreply.github.com>
2026-09-15 22:52:48 +0530

    new

---
+++ Assignments/Java/For Loops Day 04/Even_Number.java
@@ -0,0 +1,17 @@
+public class Even_Number 
+
+    {
+        public static void main (String args [])
+
+        {
+            for(int i= 1; i<=500; i++)
+
+                if (i%2 == 0) {
+                    
+                    System.out.println(i);
+                }
+
+        }
+
+    }
+
---
+++ Assignments/Java/For Loops Day 04/Seventh_Number.java
@@ -0,0 +1,13 @@
+public class Seventh_Number {
+    
+    public static void main(String args[])
+    {
+        {
+        for(int i=7; i<=200; i+=7)
+
+            System.out.println(i);    
+
+        }
+    }
+    
+}
